a72722e7bec2ae7ac214d7dcadccbfb882d4ca29
Patel-Tripti <139382407+Patel-Tripti@users.noreply.github.com>
2024-08-16 12:11:30 +0530

    Add files via upload

---
+++ Count_Vowel.java
@@ -0,0 +1,21 @@
+//Cout the vowels in the strings:
+package String;
+
+class CountVowel {
+    public static void main(String[] args) {
+        String str = "Education";
+       str = str.toLowerCase();
+       int count = 0;
+       for(int i =0;i<str.length();i++){
+        if(str.charAt(i)=='a'||
+           str.charAt(i)=='e'||
+           str.charAt(i)=='o'||
+           str.charAt(i)=='i'||
+           str.charAt(i)=='u' )
+           {
+            count++;
+       }
+      
+       } System.out.println("vowel count is = "+ count );
+    }
+}
